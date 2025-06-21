01d80b7ab7e1cd27bf25e3e1c72a849ff6774a3f
root <root@ip-172-31-25-109.ap-southeast-1.compute.internal>
2025-06-21 14:43:49 +0000

    My First Commit for Docker

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Docker!');
+});
+
+server.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
+
