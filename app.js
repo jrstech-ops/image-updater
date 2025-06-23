50fcdcac1e7b0d8e9c75c1517f3381acacc3d563
root <root@ip-172-31-25-109.ap-southeast-1.compute.internal>
2025-06-23 07:30:25 +0000

    Add or update deploy workflow with correct ECR image

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker!');
+  res.end('Hello from Docker my new version!');
 });
 
 server.listen(port, () => {
